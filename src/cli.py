2c23cad79e53e53ba10e29bab14cde00d89cf4c2
Khoa Ma <khoama@gmail.com>
2022-03-30 21:19:58 +0700

    authentication: login successful

--- src/cli.py
+++ src/cli.py
@@ -1,11 +1,22 @@
 import click
+import json
+from pathlib import Path
 from src.app.cli import app
 from src.login.cli import login
 
+CONFIG_FILE = Path.home().joinpath('.aitomatic')
+
+def load_config():
+    if CONFIG_FILE.exists():
+        return json.loads(CONFIG_FILE.read_text())
+    else:
+        return None
+
+AUTH_INFO = load_config()
 
 @click.group(help='''
     Aitomatic CLI tool to help manage aitomatic projects and apps
-''')
+''', context_settings={'obj': AUTH_INFO})
 def cli():
     pass
 
--- src/login/cli.py
+++ src/login/cli.py
@@ -1,17 +1,37 @@
 import click
 import requests
 import time
-
+import json
+from pathlib import Path
 
 ORG = 'aitomaticinc.us.auth0.com';
 CLIENT_ID = "zk9AB0KtNqJY0gVeF1p0ZmUb2tlcXpYq"
 AUDIENCE = "https://apps.aitomatic.com/dev"
-SCOPE = "openid profile email"
+SCOPE = "openid profile email offline_access"
+CONFIG_FILE = Path.home().joinpath('.aitomatic')
+
+def load_config():
+    if CONFIG_FILE.exists():
+        return json.loads(CONFIG_FILE.read_text())
+    else:
+        return None
+
+AUTH_INFO = load_config()
 
 @click.command(help='''
     Login to Aitomatic cloud
 ''')
-def login():
+@click.pass_obj
+def login(obj):
+    if obj.get("access_token") is not None or CONFIG_FILE.exists():
+        relogin = click.confirm("You're logged in. Do you want to log in again?", default=False, abort=False, prompt_suffix=': ', show_default=True, err=False)
+
+        if not relogin:
+            exit(0)
+
+    do_login()
+
+def do_login():
     click.echo('Login to Aitomatic cloud')
     device_info = request_device_code()
     display_device_info(device_info)
@@ -53,7 +73,8 @@ def display_device_info(device_info):
 
     click.echo("Waiting for authentication...")
 
-def polling_authentication(device_info):
+@click.pass_obj
+def polling_authentication(obj, device_info):
     res = requests.post(
         url="https://{}/oauth/token".format(ORG),
         data={
@@ -66,16 +87,35 @@ def polling_authentication(device_info):
     
     # response example
     # {'error': 'authorization_pending', 'error_description': 'User has yet to authorize device code.'}
-
+    # {"error": "expired_token", "error_description": "..." }
+    # {"error": "access_denied", "error_description": "..." }
+    # {"access_token": "...", "id_token": "...", "refresh_token": "..."}
     polling_data = res.json();
 
-    if polling_data['error'] == 'authorization_pending':
+    if polling_data.get('error') == 'authorization_pending':
         time.sleep(device_info['interval'])
         polling_authentication(device_info)
 
-    if polling_data['error'] == 'expired_token':
+    if polling_data.get('error') == 'expired_token' or polling_data.get('error') == 'access_denied':
         click.echo(polling_data['error_description'])
         exit(1)
 
+    if polling_data.get('access_token') is not None:
+        obj['access_token'] = polling_data['access_token']
+        obj['refresh_token'] = polling_data['refresh_token']
+        save_config({ 'at': polling_data['access_token'], 'rt': polling_data['refresh_token'], 'id': polling_data['id_token'] })
+        click.echo("Login successful!")
 
-    
+def save_config(data):
+    CONFIG_FILE.write_text(json.dumps(data))
+
+def verify_token(token):
+    res = requests.get(
+        url="https://{}/oauth/device/code".format(ORG),
+        headers={ "Authorization": "Bearer {}".format(token) }
+    )
+
+    if (res.status_code == 200):
+        return True
+
+    return False
