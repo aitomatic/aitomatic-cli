b58423167402d5542198a74abc8d09f65af44921
Khoa Ma <khoama@gmail.com>
2022-04-05 00:50:21 -0700

    feat: change login flow

--- src/app/main.py
+++ src/app/main.py
@@ -4,7 +4,7 @@ from src.app.deploy import deploy
 
 @click.group()
 def app():
-    '''CLI sub-command to help manage aitomatic apps'''
+    """CLI sub-command to help manage aitomatic apps"""
 
 
 app.add_command(deploy)
---
+++ src/login/server.py
@@ -0,0 +1,37 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+import requests
+
+
+class LoginServer(BaseHTTPRequestHandler):
+    def __init__(self, message_queue, obj, *args, **kwargs):
+        self.obj = obj
+        self.message_queue = message_queue
+        super(LoginServer, self).__init__(*args, **kwargs)
+
+    def do_GET(self):
+        o = urlparse(self.path)
+        params = parse_qs(o.query)
+        print("Params", params)
+        print("User Object", self.obj)
+
+        code = params.get("code")
+        state = params.get("state")
+
+        if params.get("code") is not None and params.get("state") is not None:
+            # validate state
+            if state[0] != self.obj.get("login_seed"):
+                print("Error: Invalid state")
+                exit(1)
+
+            # exchange code for exchange_code
+            # self.obj["exchange_code"].value = code[0]
+            self.message_queue.put(code[0])
+
+        if params.get("error") is not None:
+            print("Error: " + params.get("error_description")[0])
+            exit(1)
+
+    # bypass server log messages
+    def log_message(self, format, *args):
+        return
