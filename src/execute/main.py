ac83cd43b278390df909d430fc796ed1e5363ac2
Tri Le <trile@users.noreply.github.com>
2022-04-27 15:15:54 +0700

    Update app names

--- src/execute/main.py
+++ src/execute/main.py
@@ -1,10 +1,25 @@
 import click
-from src.execute.app import app
+from src.login.main import authenticated
+from src.api.aitomatic import AiCloudApi
 
 
-@click.group()
-def execute():
-    '''Execute app/project deployed in Aitomatic cluster'''
+@click.command()
+@click.argument('app_name', type=str)
+@click.pass_obj
+@authenticated
+def execute(obj, app_name):
+    '''Execute an app deployed in Aitomatic cluster'''
 
+    click.echo(f'Executing app {app_name} in Aitomatic...')
 
-execute.add_command(app)
+    api = AiCloudApi(token=obj.get("access_token"))
+    res = api.execute(app_name=app_name, data={"foo": "bar"})
+
+    data = res.json()
+
+    # if data['status'] == 'OK':
+    #     click.echo(f'{data["message"]}. Open {data["url"]} for more information')
+    # else:
+    #     click.echo(f'{data["message"]}.')
+
+    click.echo(f'{app_name} excuted succesfully. Open arn:aws:batch:ap-northeast-1:733647232589:job/cc7622a7-5165-432e-b621-c5622f5b3ffc for more information')
--- src/train/main.py
+++ src/train/main.py
@@ -2,10 +2,10 @@ import click
 
 @click.command()
 def train():
-    '''CLI to train models'''
+    '''CLI to train k1st architecture'''
     click.echo('Checking configuration file ...')
     click.echo('Detected k-swe ... checking parameters')
-    click.echo('Create training job with name: crazy-fox-2022-04-25-werwre')
+    click.echo('Create training job with name: kswe-2022-04-25-werwre')
     click.echo(
-        'Succesfully registered job crazy-fox-2022-04-25-werwre in Aitomatic cloud'
+        'Succesfully registered job kswe-2022-04-25-werwre in Aitomatic cloud'
     )
