f21d18bd2f6dc4785bba2b478ae3e5c0c8cb215b
Tuan Pham <phamhoangtuan.it@gmail.com>
2022-05-04 15:54:20 +0700

    feat: create AitoConfig class

--- src/run/main.py
+++ src/run/main.py
@@ -10,45 +10,51 @@ from src.execute.app import execute_app
 @authenticated
 def run():
     '''Run the app based on config file'''
-    config_data = read_aito_file()
+    aito_config = AitoConfig()
 
-    app_name = config_data['name']
-    app_config = {}
-    if config_data.get('config') is not None:
-        app_config = config_data['config']
-    elif config_data.get('config_file') is not None:
-        file_path = Path.cwd() / config_data['config_file']
-        app_config = convert_ini_config_to_dict(file_path.read_text())
-    click.echo(json.dumps(app_config))
-
-    data = execute_app(app_name=app_name, data=json.dumps(app_config))
+    data = execute_app(
+        app_name=aito_config.app_name, data=json.dumps(aito_config.app_config)
+    )
     click.echo(data)
 
 
-def read_aito_file():
-    current_dir = Path.cwd()
-    config_files = list(current_dir.glob('.aito'))
-    if len(config_files) == 0:
-        click.echo(
-            'There is no .aito config file in current folder. Please create one first.'
-        )
-        exit(1)
+class AitoConfig:
+    def __init__(self) -> None:
+        self.aito_config = self.read_aito_file()
+        self.app_name = self.aito_config.get('name')
+        if self.aito_config.get('config_file') is not None:
+            try:
+                file_path = Path.cwd() / self.aito_config['config_file']
+                self.app_config = self.convert_ini_config_to_dict(file_path.read_text())
+            except FileNotFoundError:
+                click.echo("Can't load app config file")
+                exit(1)
+        else:
+            self.app_config = {}
 
-    try:
-        return json.loads(config_files[0].read_text())
-    except json.decoder.JSONDecodeError:
-        click.echo("Can't read config file.")
-        exit(1)
+    def read_aito_file(self):
+        current_dir = Path.cwd()
+        config_files = list(current_dir.glob('.aito'))
+        if len(config_files) == 0:
+            click.echo(
+                'There is no .aito config file in current folder. Please create one first.'
+            )
+            exit(1)
 
+        try:
+            return json.loads(config_files[0].read_text())
+        except json.decoder.JSONDecodeError:
+            click.echo("Can't read config file.")
+            exit(1)
 
-def convert_ini_config_to_dict(config_content):
-    parser = ConfigParser()
-    result = {}
+    def convert_ini_config_to_dict(self, config_content):
+        parser = ConfigParser()
+        result = {}
 
-    parser.read_string(config_content)
-    for section in parser.sections():
-        result[section] = {}
-        for name, value in parser.items(section):
-            result[section][name] = value
+        parser.read_string(config_content)
+        for section in parser.sections():
+            result[section] = {}
+            for name, value in parser.items(section):
+                result[section][name] = value
 
-    return result
+        return result
