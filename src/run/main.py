69d76dc110a0c5d9526af299cf00293974b5e307
Tuan Pham <phamhoangtuan.it@gmail.com>
2022-05-04 09:46:30 +0700

    feat: check .aito file in current dir

--- src/run/main.py
+++ src/run/main.py
@@ -1,7 +1,15 @@
 import click
+from pathlib import Path
 
 
 @click.command()
 def run():
     '''Run the app based on config file'''
-    click.echo('Run')
+    current_dir = Path.cwd()
+    config_files = list(current_dir.glob('.aito'))
+    if len(config_files) == 0:
+        click.echo(
+            'There is no .aito config file in current folder. Please create one first'
+        )
+        exit(1)
+    click.echo(current_dir)
