9474ac8e40175ca3ed1af204219f22234ca5b66a
Tuan Pham <phamhoangtuan.it@gmail.com>
2022-05-04 10:14:34 +0700

    feat: use function to read aito config file

--- src/run/main.py
+++ src/run/main.py
@@ -1,10 +1,16 @@
 import click
+import json
 from pathlib import Path
 
 
 @click.command()
 def run():
     '''Run the app based on config file'''
+    config_data = read_aito_file()
+    click.echo(config_data)
+
+
+def read_aito_file():
     current_dir = Path.cwd()
     config_files = list(current_dir.glob('.aito'))
     if len(config_files) == 0:
@@ -12,4 +18,9 @@ def run():
             'There is no .aito config file in current folder. Please create one first'
         )
         exit(1)
-    click.echo(current_dir)
+
+    try:
+        return json.loads(config_files[0].read_text())
+    except json.decoder.JSONDecodeError:
+        click.echo("Can't read config file")
+        exit(1)
