a4567c0739dc34d0b8fa0b9a3b7f56973a03c577
Tuan Pham <phamhoangtuan.it@gmail.com>
2022-05-06 15:36:13 +0700

    fix: fix script to run auto complete

--- auto_complete_install.sh
+++ auto_complete_install.sh
@@ -1,28 +1,29 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 if [[ $(basename $SHELL) = 'bash' ]];
 then
+    echo "Installing bash autocompletion..."
+    echo "_AITO_COMPLETE=bash_source aito" > ~/.aito-autocompletion.sh
     if [ -f ~/.bashrc ];
     then
-        echo "Installing bash autocompletion..."
         grep -q 'aito-autocompletion' ~/.bashrc
         if [[ $? -ne 0 ]]; then
             echo "" >> ~/.bashrc
-            echo 'eval "$(_AITO_COMPLETE=bash_source aito)"' >> ~/.aito-autocompletion.sh
             echo "source ~/.aito-autocompletion.sh" >> ~/.bashrc
         fi
     fi
 elif [[ $(basename $SHELL) = 'zsh' ]];
 then
+    echo "Installing zsh autocompletion..."
+    echo "_AITO_COMPLETE=zsh_source aito" > ~/.aito-autocompletion.sh
     if [ -f ~/.zshrc ];
     then
-        echo "Installing zsh autocompletion..."
         grep -q 'aito-autocompletion' ~/.zshrc
         if [[ $? -ne 0 ]]; then
             echo "" >> ~/.zshrc
             echo "autoload bashcompinit" >> ~/.zshrc
             echo "bashcompinit" >> ~/.zshrc
-            echo 'eval "$(_AITO_COMPLETE=zsh_source aito)"' >> ~/.aito-autocompletion.sh
             echo "source ~/.aito-autocompletion.sh" >> ~/.zshrc
         fi
     fi
 fi
+source ~/.aito-autocompletion.sh
