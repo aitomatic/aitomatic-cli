e2fa9b7f73decb676543497d203c02a642a94b1d
Tuan Pham <phamhoangtuan.it@gmail.com>
2022-05-13 17:36:37 +0700

    feat: update run_app function to run app directly or from project

--- src/run/main.py
+++ src/run/main.py
@@ -21,12 +21,6 @@ def run(app_config_file: str) -> None:
         run_project(aito_config)
     else:
         run_app(aito_config)
-    
-    # if app_config_file is not None:
-    #     aito_config.set_app_config(app_config_file)
-
-    # data = trigger_app(app_name=aito_config.app_name, data=aito_config.app_config)
-    # click.echo(data)
 
 
 def run_project(aito_config: dict) -> None:
@@ -34,10 +28,7 @@ def run_project(aito_config: dict) -> None:
     apps = []
     for key in aito_config.keys():
         if 'app' in key:
-            apps.append({
-                'name': key.split(' ')[1],
-                'src': aito_config[key]['src']
-            })
+            apps.append({'name': key.split(' ')[1], 'src': aito_config[key]['src']})
     if len(apps) == 0:
         click.echo(f'Project {project_name} has no app to run')
         exit(0)
@@ -46,29 +37,43 @@ def run_project(aito_config: dict) -> None:
     else:
         click.echo(f'Project {project_name} has {len(apps)} apps. Please select one.')
         for i in range(len(apps)):
-            click.secho(f'Select {i + 1} to run app {apps[i]["name"]}.', fg='green', bold=True)
+            click.secho(
+                f'Select {i + 1} to run app {apps[i]["name"]}.', fg='green', bold=True
+            )
         choice = click.prompt(
             'Please select app to run',
-            type=click.Choice([str(i + 1) for i in range(len(apps))])
+            type=click.Choice([str(i) for i in range(1, len(apps) + 1)]),
         )
         selected_app = apps[int(choice) - 1]
 
     app_path = Path.cwd().joinpath(selected_app['src'])
     app_aito_config = read_aito_file(app_path)
-    run_app(app_aito_config)
+    run_app(app_aito_config, app_path)
 
 
-def run_app(aito_confg: dict) -> None:
-    click.echo(f'Running app {aito_confg["app"]["name"]}...')
+@click.pass_context
+def run_app(ctx, aito_confg: dict, app_path: Path = None) -> None:
+    app_name = aito_confg['app']['name']
+    click.echo(f'Running app {app_name}...')
+
+    current_dir = Path.cwd() if app_path is None else Path.cwd().joinpath(app_path)
+    config = aito_confg['app'].get('config')
+    data = {}
+    if config is not None:
+        data = read_ini_file(current_dir.joinpath(config))
+    if ctx.params['app_config_file'] is not None:
+        data = read_ini_file(Path.cwd().joinpath(ctx.params['app_config_file']))
+
+    click.echo(data)
+    res = trigger_app(app_name=app_name, data=data)
+    click.echo(res)
 
 
 def read_aito_file(folder_path: Path) -> dict:
     files = list(folder_path.glob('.aito'))
-    
+
     if len(files) == 0:
-        show_error_message(
-            '.aito file not found. Please create one first.'
-        )
+        show_error_message('.aito file not found. Please create one first.')
         exit(1)
 
     try:
