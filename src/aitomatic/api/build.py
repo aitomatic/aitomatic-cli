9508152515c744dd212107e1daf371292876f949
Nubs01 <roshan.nanu@gmail.com>
2023-01-15 20:47:32 -0500

    chore: formatting

--- src/aitomatic/api/build.py
+++ src/aitomatic/api/build.py
@@ -37,7 +37,6 @@ class ModelBuilder:
         params = model_input.get('model_params')
         return params
 
-
     def build_model(self, model_type: str, model_name: str,
                     knowledge_set_name: str,
                     data_set_name: str, model_params: dict):
@@ -57,9 +56,12 @@ class ModelBuilder:
             'data_set_name': data_set_name,
             'model_params': model_params
         }
+        return payload
+    '''
         resp = self.project.make_request('post', self.MODEL_BUILD,
                                          json=payload)
         return resp
+    '''
 
     def get_base_model_params(self, model_type: str, knowledge_set_name: str):
         knowledge = self.project.get_knowledge(knowledge_set_name)
