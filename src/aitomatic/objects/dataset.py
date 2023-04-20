495621e43937342d9fae6280a8071fc0642cd702
Vo Phi Hung <vphungkhtn@gmail.com>
2023-04-20 09:44:38 +0700

    Deploy dev to main (#40)
    * Feat: adapt new project structure
    
    * feat: Tuning with new structure
    
    * feat: Add model output

---
+++ src/aitomatic/objects/dataset.py
@@ -0,0 +1,18 @@
+class Dataset:
+    raw_data: dict
+    def __init__(self, raw_data) -> None:
+        self.raw_data = raw_data
+
+    @property
+    def type(self) -> str:
+        return self.raw_data.get('type', '')
+    
+    @property
+    def metadata(self) -> dict:
+        if self.type == 'DATA':
+            return self.raw_data.get('dataset_metadata', {}).get('metadata', {})
+        if self.type == 'DESCRIBE':
+            obj = {}
+            for var in self.raw_data.get('variables'):
+                obj[var['name']] = var
+            return obj
